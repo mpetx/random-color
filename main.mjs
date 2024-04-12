10762832502a8526f8f57a70a0d61feb9eac9f74
mpetx <143336693+mpetx@users.noreply.github.com>
2024-04-13 02:39:56 +0900

    色のランダムさを向上

--- main.mjs
+++ main.mjs
@@ -1,24 +1,86 @@
 
+const engamma = (component) => {
+    if (component <= 0.0031308)
+	return 12.92 * component;
+    else
+	return 1.055 * Math.pow(component, 1 / 2.4) - 0.055;
+};
+
+const degamma = (component) => {
+    if (component <= 0.04045)
+	return component / 12.92;
+    else
+	return Math.pow((component + 0.055) / 1.055, 2.4);
+};
+
 class Color {
 
-    constructor(r, g, b) {
-	this.r = r;
-	this.g = g;
-	this.b = b;
+    constructor(lr, lg, lb) {
+	this.lr = lr;
+	this.lg = lg;
+	this.lb = lb;
     }
 
-    isLight() {
-	const luminosity = (this.r + this.g + this.b) / 3;
-	return luminosity >= 128;
+    get r() {
+	return Math.round(engamma(this.lr) * 255);
+    };
+
+    get g() {
+	return Math.round(engamma(this.lg) * 255);
+    };
+
+    get b() {
+	return Math.round(engamma(this.lb) * 255);
+    };
+
+    get luminosity() {
+	return engamma(this.lr * 0.2126 + this.lg * 0.7152 + this.lb * 0.0722);
+    }
+
+    get isLight() {
+	return this.luminosity > 0.5;
     }
 }
 
+const labToXYZ = (l, a, b) => {
+    const f = (x) => {
+	if (x > 6 / 29)
+	    return x * x * x;
+	else
+	    return 3 * 6 * 6 / (29 * 29) * (x - 4 / 29);
+    };
+    return [
+	0.9505 * f((l + 16) / 116 + a / 500),
+	1 * f((l + 16) / 116),
+	1.089 * f((l + 16) / 116 - b / 200)
+    ];
+};
+
+const xyzToRGB = (x, y, z) => {
+    return [
+	3.2406254773201 * x -1.5372079722103 * y -0.49862859869825 * z,
+	-0.96893071472932 * x + 1.8757560608852 * y + 0.041517523842954 * z,
+	0.055710120445511 * x -0.20402105059849 * y + 1.0569959422544 * z
+    ];
+};
+
+const isValidRGB = (r, g, b) => {
+    return 0 <= r && r <= 1 && 0 <= g && g <= 1 && 0 <= b && b <= 1;
+};
+
+const tryCount = 50;
+
 const generateRandomColor = () => {
-    return new Color(
-	Math.round(Math.random() * 255),
-	Math.round(Math.random() * 255),
-	Math.round(Math.random() * 255)
-    );
+    for (let i = 0; i < tryCount; ++i) {
+	const l = Math.random() * 100;
+	const aStar = Math.random() * 250 - 125;
+	const bStar = Math.random() * 250 - 125;
+	const [x, y, z] = labToXYZ(l, aStar, bStar);
+	const [r, g, b] = xyzToRGB(x, y, z);
+	if (isValidRGB(r, g, b))
+	    return new Color(r, g, b);
+    }
+    return null;
 };
 
 const hexDigits = [
@@ -45,7 +107,7 @@ const createColorDisplay = (color) => {
     const colorCode = toColorCode(color);
     colorDisplay.style.backgroundColor = colorCode;
     colorDisplayCode.textContent = colorCode;
-    colorDisplayCode.style.color = color.isLight() ? "black" : "white";
+    colorDisplayCode.style.color = color.isLight ? "black" : "white";
     return colorDisplayFragment;
 };
 
@@ -56,6 +118,8 @@ const generateButtonClickListener = () => {
     colorContainer.innerHTML = "";
     for (let i = 0; i < colorCount; ++i) {
 	const color = generateRandomColor();
+	if (color === null)
+	    continue;
 	const colorDisplay = createColorDisplay(color);
 	colorContainer.appendChild(colorDisplay);
     }
